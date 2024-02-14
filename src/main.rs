49b44b32f78f7bddac0eab84ca3888511fc7b68a
WayneWeng95 <wayneweng9508@gmail.com>
2024-02-14 15:49:31 +0100

    Mounting successful but funcion not implemented

--- src/main.rs
+++ src/main.rs
@@ -245,9 +245,9 @@ fn fuse_main() {
     let filesystem = MyFS;
     let mut options = vec![MountOption::RW, MountOption::FSName("hello".to_string())];
 
-    // options.push(MountOption::AutoUnmount);
+    options.push(MountOption::AllowOther);
 
-    // options.push(MountOption::AllowRoot);
+    options.push(MountOption::AutoUnmount);
 
     fuser::mount2(filesystem, mountpoint, &options).unwrap();
 }
